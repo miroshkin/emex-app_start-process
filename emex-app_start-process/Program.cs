518ef22435f354bb4bbc77f5136b0c7d6e5aeb02
Anton Miroshkin <anton.miroshkin@gmail.com>
2020-01-31 17:33:53 +0300

    Added project

---
+++ emex-app_start-process/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emex_app_start_process
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process.Start(args[0]);
+        }
+    }
+}
